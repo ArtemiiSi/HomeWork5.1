b12714e975b624f9aea19082840f23f44d0f9e4c
ArtemiiSi <107568989+ArtemiiSi@users.noreply.github.com>
2023-04-03 08:14:45 +0300

    change

--- src/test/java/BonusSeviceTest.java
+++ src/test/java/BonusSeviceTest.java
@@ -23,4 +23,21 @@ public class BonusSeviceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void shouldCalculateForNotRegisteredAndUnderLimit() {
+        BonusService service = new BonusService();
+        long amount = 1000;
+        boolean registered = false;
+        long expected = 10;
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+    }
+    void shouldCalculateForNotRegisteredAndOverLimit() {
+        BonusService service = new BonusService();
+        long amount = 1_000_000;
+        boolean registered = false;
+        long expected = 500;
+        long actual = service.calculate(amount, registered);
+        Assertions.assertEquals(expected, actual);
+    }
 }
